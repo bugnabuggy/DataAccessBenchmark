8eeea42f9f594c8ba255a5686d24fbfcea8341e9
LICH010 <LICH010@yandex.ru>
2018-02-27 17:58:38 +0600

    change

--- benchmarkUI/src/app/tests/session.component.spec.ts
+++ benchmarkUI/src/app/tests/session.component.spec.ts
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { SessionComponent } from '../components/session.component';
+=======
+import { TestBed, async } from '@angular/core/testing';
+import { SessionComponent} from '../components.ts/session.component';
+>>>>>>> parent of 92df522... Revert "typesetting history and session html"
 import {MatButtonModule,
     MatPaginatorModule,
     MatTableModule,
@@ -9,6 +14,7 @@ import {MatButtonModule,
     MatListModule,
     MatDividerModule,
     MatProgressSpinnerModule} from '@angular/material';
+<<<<<<< HEAD
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HTTPService } from '../services/httpService';
 import { HttpModule } from '@angular/http';
@@ -28,6 +34,13 @@ describe('SessionComponent', () => {
   beforeEach(async(() => {
     const httpServiceStub = jasmine.createSpyObj('HTTPService',['serverFeatures'])
     getQuoteSpy = httpServiceStub.serverFeatures.and.returnValue(testQuote )
+=======
+    import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+    import { HTTPService } from '../services/httpService';
+    import { HttpModule } from '@angular/http';
+describe('SessionComponent', () => {
+  beforeEach(async(() => {
+>>>>>>> parent of 92df522... Revert "typesetting history and session html"
     TestBed.configureTestingModule({
       declarations: [
         SessionComponent
@@ -45,6 +58,7 @@ describe('SessionComponent', () => {
         MatSelectModule,
         BrowserAnimationsModule
       ],
+<<<<<<< HEAD
       providers: [{provide: HTTPService, useValue: httpServiceStub }],
     }).compileComponents();
   }));
@@ -59,3 +73,20 @@ describe('when test with synchronous observable', () => {
 
 });
 });
+=======
+      providers: [HTTPService],
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(SessionComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(SessionComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  }));
+});
+>>>>>>> parent of 92df522... Revert "typesetting history and session html"
