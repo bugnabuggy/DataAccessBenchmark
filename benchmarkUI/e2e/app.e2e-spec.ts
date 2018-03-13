8dc6e0d797738d9a5863c2a2507646557c7d528d
LICH010 <LICH010@yandex.ru>
2018-03-13 17:17:20 +0600

    add e2e test

--- benchmarkUI/e2e/app.e2e-spec.ts
+++ benchmarkUI/e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { tick } from '@angular/core/testing';
 
 describe('benchmark-ui App', () => {
   let page: AppPage;
@@ -7,8 +8,40 @@ describe('benchmark-ui App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should clear table history', () => {
+    debugger
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    page.setInputForButtonFill();
+    page.clickButtonFill()
+    page.clickTabsHistory();
+    let checkCountRecords = page.countRowRecords()
+    expect(checkCountRecords).not.toEqual(0)
+    page.clickButtonClearHistory();
+    page.tick();
+    let countRecord = page.countRowRecords();
+    expect(countRecord).toEqual(0);
   });
+
+  it('should add table all operation and check in tables history and session', () => {
+    page.navigateTo();
+    page.clickTabsHistory();
+    let before = page.countRowRecords();
+    page.clickTabsSession();
+    page.setInputForButtonFill();
+    page.clickButtonFill()
+    page.setInputForButtonDelete();
+    page.clickButtonDeleteEF()
+    page.clickButtonDeleteSQL()
+    page.clickButtonSelectEF();
+    page.clickButtonSelectSQL();
+    page.clickButtonFlushEF();
+    page.clickButtonFill();
+    page.clickButtonFlushSQL();
+    page.clickTabsHistory();
+    page.tick()
+    let afte = page.countRowRecords();
+    expect(before).not.toEqual(afte);
+  });
+
+
 });
--- benchmarkUI/e2e/app.po.ts
+++ benchmarkUI/e2e/app.po.ts
@@ -5,7 +5,67 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+
+  clickTabsHistory(){
+    return element.all(by.css('a.mat-tab-link')).get(1).click();
   }
+
+  clickTabsSession(){
+    return element.all(by.css('a.mat-tab-link')).get(0).click();
+  }
+
+  clickButtonFill(){
+    return element(by.css('button')).click();
+  }
+
+  clickButtonClearHistory(){
+    return element(by.css('button')).click();
+  }
+
+  clickButtonDeleteEF(){
+    return element.all(by.css('button')).get(1).click();
+  }
+
+  clickButtonDeleteSQL(){
+    return element.all(by.css('button')).get(2).click();
+  }
+  clickButtonSelectEF(){
+    return element.all(by.css('button')).get(3).click();
+  }
+
+  clickButtonSelectSQL(){
+    return element.all(by.css('button')).get(4).click();
+  }
+
+  clickButtonFlushEF(){
+    return element.all(by.css('button')).get(5).click();
+  }
+
+  clickButtonFlushSQL(){
+    return element.all(by.css('button')).get(6).click();
+  }
+
+  setInputForButtonFill(){
+    return element(by.css('input')).sendKeys(20);
+  }
+
+  setInputForButtonDelete(){
+    return element.all(by.css('input')).get(1).sendKeys(5);
+  }
+
+  
+
+ 
+  tick(){
+    return browser.waitForAngular();
+  }
+
+ 
+
+  countRowRecords(){
+    let test = element.all(by.tagName('mat-row'));
+    return test.count();
+  }
+
+
 }
